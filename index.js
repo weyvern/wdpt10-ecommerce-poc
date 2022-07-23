eb225d3994a549b0d78aa26bc63cd15bc899271b
Jorge Paul <19370560+weyvern@users.noreply.github.com>
2022-07-23 10:34:15 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const users = [
+  {
+    id: 1,
+    name: "Carlos",
+    email: "carlos@mail.com",
+    cart: []
+  },
+  {
+    id: 2,
+    name: "Patricia",
+    email: "patricia@mail.com",
+    cart: []
+  },
+  {
+    id: 3,
+    name: "Julian",
+    email: "julian@mail.com",
+    cart: []
+  },
+  {
+    id: 4,
+    name: "Shir",
+    email: "shir@mail.com",
+    cart: []
+  },
+  {
+    id: 5,
+    name: "Sadef",
+    email: "sadef@mail.com",
+    cart: []
+  },
+  {
+    id: 6,
+    name: "Andrea",
+    email: "andrea@mail.com",
+    cart: []
+  },
+  {
+    id: 7,
+    name: "Oktay",
+    email: "oktay@mail.com",
+    cart: []
+  }
+];
+
+const products = [];
+
+const orders = [];
+
+/* 
+
+*/
